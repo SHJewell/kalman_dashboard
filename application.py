c6209e8c0f205e099e5158d57a6d17d48b3964e3
SHJewell <scott.hjewell@gmail.com>
2023-02-28 09:49:24 -0800

    Initial commit

---
+++ application.py
@@ -0,0 +1,221 @@
+import dash
+from dash import html as dhtml
+from dash import dcc, Input, Output, State
+from dash.dependencies import Input, Output, State
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import dash_bootstrap_components as dbc
+
+#non-plotly imports
+import numpy as np
+
+#local imports
+import kalman_filter as kal
+
+'''
+========================================================================================================================
+Data
+'''
+
+
+'''
+========================================================================================================================
+Dashboard
+
+'''
+
+graph_config = {'modeBarButtonsToRemove' : ['hoverCompareCartesian', 'select2d', 'lasso2d'],
+                'doubleClick':  'reset+autosize', 'toImageButtonOptions': { 'height': None, 'width': None, },
+                'displaylogo': False}
+
+colors = {'background': '#111111', 'text': '#7FDBFF'}
+
+app = dash.Dash(__name__,
+                meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
+                external_stylesheets=[dbc.themes.SLATE])
+#application = app.server
+app.title = 'Kalman filter simulation'
+
+
+(state, est_state, meas) = kal.eval_filter(20, 0.1, 0.1)
+plot = go.Figure(data=go.Scatter(x=state[:, 0], y=state[:, 2], name='True State', mode='lines+markers'))
+plot.add_scatter(x=est_state[:, 0], y=est_state[:, 2], name='Filtered State', mode='lines+markers')
+plot.add_scatter(x=meas[:, 0], y=meas[:, 1], name='Observed', mode='markers')
+plot.update_layout(paper_bgcolor='#515960', plot_bgcolor='#515960',
+                 font_color='white',
+                 margin=dict(l=5, r=5, t=5, b=5))
+plot.update_xaxes(title_text='x[n]')
+plot.update_yaxes(title_text='y[n]')
+
+graph_tab = dbc.Card(
+    dbc.CardBody(id='graph-card',
+        children=[
+            dbc.Col([
+            ]),
+            dbc.Row([dhtml.H2(f'Kalman Filter', id='loc_label')]),
+            dbc.Row([
+                dcc.Loading(dcc.Graph(id='time_series', figure=plot))
+            ])
+        ]
+    )
+)
+
+kal_mag = np.sqrt(np.square(np.abs(est_state[:, 2] + est_state[:, 0])))
+obs_mag = np.sqrt(np.square(np.abs(meas[:, 1] + meas[:, 0])))
+state_mag = np.sqrt(np.square(np.abs(state[:, 2] + state[:, 0])))
+
+kal_rmse = np.sqrt(np.sum(np.square(kal_mag - state_mag)) / len(state))
+obs_rmse = np.sqrt(np.sum(np.square(obs_mag - state_mag)) / len(state))
+
+diffs = make_subplots(rows=3, cols=1)
+x = np.array(range(len(state[:, 0])))
+
+diffs.add_trace(go.Scatter(x=x, y=np.abs(state[:, 2] - est_state[:, 2]), name='Filter x Error', mode='markers'), row=1, col=1)
+diffs.add_trace(go.Scatter(x=x, y=np.abs(meas[:, 1] - state[:, 2]), name='Meas. x Error', mode='markers'), row=1, col=1)
+diffs.add_trace(go.Scatter(x=x, y=np.abs(state[:, 0] - est_state[:, 0]), name='Filter y Error', mode='markers'), row=2, col=1)
+diffs.add_trace(go.Scatter(x=x, y=np.abs(meas[:, 0] - state[:, 0]), name='Meas. y Error', mode='markers'), row=2, col=1)
+diffs.add_trace(go.Scatter(x=x, y=kal_mag, name='Filter Norm Error', mode='markers'), row=3, col=1)
+diffs.add_trace(go.Scatter(x=x, y=obs_mag, name='Meas. Norm Error', mode='markers'), row=3, col=1)
+diffs.update_layout(paper_bgcolor='#515960', plot_bgcolor='#515960',
+                 font_color='white',
+                 margin=dict(l=5, r=5, t=5, b=5))
+diffs.update_xaxes(title_text='Sample Number', row=2, col=1)
+diffs.update_yaxes(title_text='Difference', row=1, col=1)
+diffs.update_yaxes(title_text='Difference', row=2, col=1)
+
+analysis_tab = dbc.Card(
+    dbc.CardBody(id='analysis-card',
+        children=[
+            dbc.Col([
+            ]),
+            dbc.Row([dhtml.H2(f'Errors')]),
+            dbc.Row([
+                dcc.Loading(dcc.Graph(id='difference', figure=diffs))
+            ])
+        ]
+    )
+)
+
+controls_tab = dbc.Card(
+    dbc.CardBody(id='controls-card',
+        children=[
+            dbc.Col([
+                dhtml.H5('Number of Samples:'),
+                dcc.Textarea(id='n_samples', value=f'{20}', style={'height': 30, 'width': '100%'}),
+                dhtml.H5('x-axis Noise'),
+                dcc.Textarea(id='x_sigma', value=f'{0.1}', style={'height': 30, 'width': '100%'}),
+                dhtml.H5('y-axis Noise'),
+                dcc.Textarea(id='y_sigma', value=f'{0.1}', style={'height': 30, 'width': '100%'}),
+                dhtml.H5('Initial condition T'),
+                dcc.Textarea(id='T', value=f'{1}', style={'height': 30, 'width': '100%'}),
+                dhtml.H5('Initial condition sigma2'),
+                dcc.Textarea(id='sig2', value=f'{0.3}', style={'height': 30, 'width': '100%'}),
+                dhtml.Button('Refresh', id='redraw', n_clicks=0),
+                #dhtml.Spacer,
+                dhtml.H5(f'Kalman Err: {kal_rmse:.4f}', id='kal-err'),
+                dhtml.H5(f'Obs. Err: {obs_rmse:.4f}', id='obs-err')
+            ])
+        ]
+    )
+)
+
+map_page = dbc.Card(
+    dbc.CardBody(id='map_container',
+        children=[
+            dbc.Row([
+                dbc.Col(graph_tab, width=5),
+                dbc.Col(analysis_tab, width=5),
+                dbc.Col(controls_tab, width=2)
+            ])
+        ]
+    )
+)
+
+
+app.layout = dhtml.Div([
+    dbc.CardBody(
+        id='main_card',
+        children=[dbc.Card(map_page),
+                  dcc.Link('By SHJewell', href=f'https://shjewell.com'),
+                  dhtml.H6(f'Built using Python and Plotly Dash'),
+                  dcc.Link(f'Based on Kalman filter implementation by Marko Cotra',
+                           href=f'https://medium.com/towards-data-science/wtf-is-sensor-fusion-part-2-the-good-old-kalman-filter-3642f321440')
+                  ]
+    )
+])
+
+'''
+========================================================================================================================
+Callbacks
+'''
+
+@app.callback(
+    [Output('time_series', 'figure'),
+     Output('difference', 'figure'),
+     Output('kal-err', 'children'),
+     Output('obs-err', 'children')],
+    Input('redraw', 'n_clicks'),
+    [State('n_samples', 'value'),
+     State('x_sigma', 'value'),
+     State('y_sigma', 'value'),
+     State('T', 'value'),
+     State('sig2', 'value')]
+)
+
+def refilter(_, N, sig_x, sig_y, T, sig2):
+
+    try:
+        (state, est_state, meas) = kal.eval_filter(int(N), float(sig_x), float(sig_y), float(T), float(sig2))
+    except ValueError:
+        return dash.no_update
+
+    plot = go.Figure(data=go.Scatter(x=state[:, 0], y=state[:, 2], name='True State', mode='lines+markers'))
+    plot.add_scatter(x=est_state[:, 0], y=est_state[:, 2], name='Filtered State', mode='lines+markers')
+    plot.add_scatter(x=meas[:, 0], y=meas[:, 1], name='Observed', mode='markers')
+    plot.update_layout(paper_bgcolor='#515960', plot_bgcolor='#515960',
+                     font_color='white',
+                     margin=dict(l=5, r=5, t=5, b=5))
+    plot.update_xaxes(title_text='x[n]')
+    plot.update_yaxes(title_text='y[n]')
+
+    kal_mag = np.sqrt(np.square(np.abs(est_state[:, 2] + est_state[:, 0])))
+    obs_mag = np.sqrt(np.square(np.abs(meas[:, 1] + meas[:, 0])))
+    state_mag = np.sqrt(np.square(np.abs(state[:, 2] + state[:, 0])))
+
+    kal_rmse = np.sqrt(np.sum(np.square(kal_mag - state_mag)) / len(state))
+    obs_rmse = np.sqrt(np.sum(np.square(obs_mag - state_mag)) / len(state))
+
+    diffs = make_subplots(rows=3, cols=1)
+    x = np.array(range(len(state[:, 0])))
+    kal_mag = np.sqrt(
+        np.square(np.abs(state[:, 2] - est_state[:, 2])) + np.square(np.abs(state[:, 0] - est_state[:, 0])))
+    obs_mag = np.sqrt(np.square(np.abs(meas[:, 1] - state[:, 2])) + np.square(np.abs(meas[:, 0] - state[:, 0])))
+    diffs.add_trace(go.Scatter(x=x, y=np.abs(state[:, 2] - est_state[:, 2]), name='Filter x Error', mode='markers'),
+                    row=1, col=1)
+    diffs.add_trace(go.Scatter(x=x, y=np.abs(meas[:, 1] - state[:, 2]), name='Meas. x Error', mode='markers'), row=1,
+                    col=1)
+    diffs.add_trace(go.Scatter(x=x, y=np.abs(state[:, 0] - est_state[:, 0]), name='Filter y Error', mode='markers'),
+                    row=2, col=1)
+    diffs.add_trace(go.Scatter(x=x, y=np.abs(meas[:, 0] - state[:, 0]), name='Meas. y Error', mode='markers'), row=2,
+                    col=1)
+    diffs.add_trace(go.Scatter(x=x, y=kal_mag, name='Filter Norm Error', mode='markers'), row=3, col=1)
+    diffs.add_trace(go.Scatter(x=x, y=obs_mag, name='Meas. Norm Error', mode='markers'), row=3, col=1)
+    diffs.update_layout(paper_bgcolor='#515960', plot_bgcolor='#515960',
+                        font_color='white',
+                        margin=dict(l=5, r=5, t=5, b=5))
+    diffs.update_xaxes(title_text='Sample Number [n]', row=3, col=1)
+    diffs.update_yaxes(title_text='x[n] Difference', row=1, col=1)
+    diffs.update_yaxes(title_text='y[n] Difference', row=2, col=1)
+    diffs.update_yaxes(title_text='Magnitude[n] diff', row=3, col=1)
+
+    kal_err = f'Filter error: {kal_rmse:.3f}'
+    obs_err = f'Obs. Error {obs_rmse:.3f}'
+
+
+    return plot, diffs, kal_err, obs_err
+
+if __name__ == '__main__':
+    #app.run_server(debug=True, port=8080)
+    app.run(debug=True, port=8080)
+    #application.run(port=8080)
+
---
+++ kalman_filter.py
@@ -0,0 +1,148 @@
+
+'''
+Kalman Filter implementation by Marko Cotra from:
+https://medium.com/towards-data-science/wtf-is-sensor-fusion-part-2-the-good-old-kalman-filter-3642f321440
+'''
+
+import numpy as np
+import time
+
+class MotionModel():
+    def __init__(self, A, Q):
+        self.A = A
+        self.Q = Q
+
+        (m, _) = Q.shape
+        self.zero_mean = np.zeros(m)
+
+    def __call__(self, x):
+        new_state = self.A @ x + np.random.multivariate_normal(self.zero_mean, self.Q)
+        return new_state
+
+
+class MeasurementModel():
+    def __init__(self, H, R):
+        self.H = H
+        self.R = R
+
+        (n, _) = R.shape
+        self.zero_mean = np.zeros(n)
+
+    def __call__(self, x):
+        measurement = self.H @ x + np.random.multivariate_normal(self.zero_mean, self.R)
+        return measurement
+
+
+class system_sim():
+
+    def __init__(self, s2_x=0.1, s2_y=0.1, T=1, lambda2=0.3):
+        self.s2_x = s2_x ** 2
+        self.s2_y = s2_y ** 2
+        self.T = T
+        self.lambda2 = lambda2 ** 2
+
+    def create_model_parameters(self):
+
+        # Motion model parameters
+        F = np.array([[1, self.T],
+                      [0, 1]])
+        base_sigma = np.array([[self.T ** 3 / 3, self.T ** 2 / 2],
+                               [self.T ** 2 / 2, self.T]])
+
+        sigma_x = self.s2_x * base_sigma
+        sigma_y = self.s2_y * base_sigma
+
+        zeros_2 = np.zeros((2, 2))
+        A = np.block([[F, zeros_2],
+                      [zeros_2, F]])
+        Q = np.block([[sigma_x, zeros_2],
+                      [zeros_2, sigma_y]])
+
+        # Measurement model parameters
+        H = np.array([[1, 0, 0, 0],
+                      [0, 0, 1, 0]])
+        R = self.lambda2 * np.eye(2)
+
+        return A, H, Q, R
+
+
+    def simulate_system(self, K, x0):
+        (A, H, Q, R) = self.create_model_parameters()
+
+        # Create models
+        motion_model = MotionModel(A, Q)
+        meas_model = MeasurementModel(H, R)
+
+        (m, _) = Q.shape
+        (n, _) = R.shape
+
+        state = np.zeros((K, m))
+        meas = np.zeros((K, n))
+
+        # initial state
+        x = x0
+        for k in range(K):
+            x = motion_model(x)
+            z = meas_model(x)
+
+            state[k, :] = x
+            meas[k, :] = z
+
+        return state, meas
+
+
+class KalmanFilter():
+    def __init__(self, A, H, Q, R, x_0, P_0):
+        # Model parameters
+        self.A = A
+        self.H = H
+        self.Q = Q
+        self.R = R
+
+        # Initial state
+        self._x = x_0
+        self._P = P_0
+
+    def predict(self):
+        self._x = self.A @ self._x
+        self._P = self.A @ self._P @ self.A.transpose() + self.Q
+
+    def update(self, z):
+        self.S = self.H @ self._P @ self.H.transpose() + self.R
+        self.V = z - self.H @ self._x
+        self.K = self._P @ self.H.transpose() @ np.linalg.inv(self.S)
+
+        self._x = self._x + self.K @ self.V
+        self._P = self._P - self.K @ self.S @ self.K.transpose()
+
+    def get_state(self):
+        return self._x, self._P
+
+def eval_filter(K, sig_x, sig_y, T=1, lambda2=0.3):
+
+    our_system = system_sim(s2_x=sig_x, s2_y=sig_y, T=T, lambda2=lambda2)
+
+    t = time.monotonic()
+    np.random.seed(int(str(t)[-3:]))
+
+    (A, H, Q, R) = our_system.create_model_parameters()
+
+    # initial state
+    x = np.array([0, 0.1, 0, 0.1])
+    P = 0 * np.eye(4)
+
+    (state, meas) = our_system.simulate_system(K, x)
+    kalman_filter = KalmanFilter(A, H, Q, R, x, P)
+
+    est_state = np.zeros((K, 4))
+    est_cov = np.zeros((K, 4, 4))
+
+    for k in range(K):
+        kalman_filter.predict()
+        kalman_filter.update(meas[k, :])
+        (x, P) = kalman_filter.get_state()
+
+        est_state[k, :] = x
+        est_cov[k, ...] = P
+
+    return state, est_state, meas

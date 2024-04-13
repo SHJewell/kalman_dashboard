cb9eff1517c4864eaea2a3c49560fd7559d75985
SHJewell <scott.hjewell@gmail.com>
2024-04-13 10:38:17 -0700

    updating deploy

--- src/app.py
+++ src/app.py
@@ -265,7 +265,7 @@ def toggle_collapse(n, is_open):
 
 
 if __name__ == '__main__':
-    #app.run_server(debug=True, port=8080)
-    app.run(debug=True, port=8080)
+    app.run_server(debug=True, port=8080)
+    # app.run(debug=True, port=8080)
     #application.run(port=8080)
 
